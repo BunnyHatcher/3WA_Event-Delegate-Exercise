91ba9a592c6ac7056ac97d16a32eeb850b43f5fc
Martin Engelhaus <martinengelhaus@gmx.de>
2022-10-11 12:37:31 +0200

    improved shooting method

--- UnityEvents_Exercise/Assets/Scripts/ShootBehavior.cs
+++ UnityEvents_Exercise/Assets/Scripts/ShootBehavior.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class ShootBehavior : MonoBehaviour
 {
     [SerializeField] GameObject _bulletPrefab;
-
-    [SerializeField] GameObject _canon;
-
-    [SerializeField] float bulletSpeed;
+    [SerializeField] float _bulletSpeed;
+    [SerializeField] Transform _bulletGroup;
 
     //Setting shot intervall
     [SerializeField] float _delayBetweenShots = 0.2f;
@@ -20,8 +18,32 @@ public class ShootBehavior : MonoBehaviour
 
     private void FireBullet()
     {
-        GameObject newBullet = Instantiate(_bulletPrefab, gameObject.transform.position, Quaternion.identity);
-        newBullet.GetComponent<BulletMovement>().Shoot(bulletSpeed);
+        // create Vector2 for all 4 directions
+        Vector2[] _directions = new Vector2[4]
+        {
+
+            Vector2.right,
+            Vector2.left,
+            Vector2.up,
+            Vector2.down
+
+        };
+
+        // create bullets in all 4 directions via for loop
+        for (int i = 0; i < 4; i++)
+        {   
+            //create bullets via Instantiate at the transform of the game object the script is added to
+            GameObject newBullet = Instantiate(_bulletPrefab, gameObject.transform.position, Quaternion.identity);
+            
+            //center instanciated bullet at centre of instanciater game object
+            //newBullet.transform.localPosition = Vector2.zero;
+            
+            //give bullets velocity via Add force
+            newBullet.GetComponent<Rigidbody2D>().AddForce(_directions[i] * _bulletSpeed);
+            
+            //add instanciated bullets to transform of parent in hierarchy
+            newBullet.transform.parent = _bulletGroup;
+        }
     }
 
     private void Awake()
